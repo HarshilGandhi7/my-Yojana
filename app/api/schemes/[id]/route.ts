33e503ec9a669cbc56803016d9463f25012f6621
HarshilGandhi7 <harshil.tejash2022@vitstudent.ac.in>
2025-10-19 15:47:37 +0530

    update GET handler to use NextRequest

--- app/api/schemes/[id]/route.ts
+++ app/api/schemes/[id]/route.ts
@@ -1,15 +1,18 @@
 import clientPromise from "@/lib/mongodb";
+import { NextRequest } from "next/server";
 
 export async function GET(
-  request: Request,
-  context: { params: { id: string } }
+  request: NextRequest,
+  {params}: { params: { id: string } }
 ) {
-  const {id}=context.params;
+  const { id } = params;
   try {
     const client = await clientPromise;
-    const dbName = process.env.MONGODB_DB ;
+    const dbName = process.env.MONGODB_DB;
     const db = client.db(dbName);
-    const scheme = await db.collection("schemes").findOne({ _id: id as String});
+    const scheme = await db
+      .collection("schemes")
+      .findOne({ _id: id as String });
 
     if (!scheme) {
       return new Response("Scheme not found", { status: 404 });
@@ -20,10 +23,10 @@ export async function GET(
     });
   } catch (error) {
     return new Response(
-      JSON.stringify({ 
-        error: "Failed to fetch scheme", 
-        message: error instanceof Error ? error.message : String(error) 
-      }), 
+      JSON.stringify({
+        error: "Failed to fetch scheme",
+        message: error instanceof Error ? error.message : String(error),
+      }),
       {
         status: 500,
         headers: { "Content-Type": "application/json" },
